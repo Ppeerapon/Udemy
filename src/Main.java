a1c49d71192f2d7a09afa903a0155b640e4866f5
zero <pee13070mn091@gmail.com>
2021-08-12 21:23:54 +0700

    Test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //SpeedConverter.printConversion(25.42);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+
+    }
+
+}
